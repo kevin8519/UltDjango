1dd0feeffa906a2d21eee1c2d99f51ad0a46d2ab
kevin8519 <thanakevin85@gmail.com>
2016-09-30 14:53:09 -0400

    Settings and database configuration has been done. Secret key has been moved out

---
+++ UltDjango/.bash_profile
@@ -0,0 +1,2 @@
+export ENV_ROLE=development
+export CRMEASY_DB_PASS=myPassword
--- UltDjango/settings.py
+++ UltDjango/settings.py
@@ -11,19 +11,26 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from secret import *
+
+from django.core.exceptions import ImproperlyConfigured
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pmusn$@_)@y8u3r43(%u7%qqqkkpq8ao7ssiwqy1)8r&l1$83('
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
@@ -75,8 +82,12 @@ WSGI_APPLICATION = 'UltDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD':CRMEASY_DB_PASS,
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
